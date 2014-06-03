22e6aabfc31e903cc105040dd75da0221048e67b
Dan Carley <dan.carley@gmail.com>
2014-06-03 13:39:03 +0100

    Empty tests for stale and mirror failover.

--- cdn_acceptance_test.go
+++ cdn_acceptance_test.go
@@ -56,3 +56,39 @@ func TestHeaderUnspoofableClientIP(t *testing.T) {
 func TestHeaderHostUnmodified(t *testing.T) {
 	t.Error("Not implemented")
 }
+
+// Should serve stale object and not hit mirror(s) if origin is down and
+// object is beyond TTL but still in cache.
+func TestFailoverOriginDownServeStale(t *testing.T) {
+	t.Error("Not implemented")
+}
+
+// Should serve stale object and not hit mirror(s) if origin returns a 5xx
+// response and object is beyond TTL but still in cache.
+func TestFailoverOrigin5xxServeStale(t *testing.T) {
+	t.Error("Not implemented")
+}
+
+// Should fallback to first mirror if origin is down and object is not in
+// cache (active or stale).
+func TestFailoverOriginDownUseFirstMirror(t *testing.T) {
+	t.Error("Not implemented")
+}
+
+// Should fallback to first mirror if origin returns 5xx response and object
+// is not in cache (active or stale).
+func TestFailoverOrigin5xxUseFirstMirror(t *testing.T) {
+	t.Error("Not implemented")
+}
+
+// Should fallback to second mirror if both origin and first mirror are
+// down.
+func TestFailoverOriginDownFirstMirrorDownUseSecondMirror(t *testing.T) {
+	t.Error("Not implemented")
+}
+
+// Should fallback to second mirror if both origin and first mirror return
+// 5xx responses.
+func TestFailoverOrigin5xxFirstMirror5xxUseSecondMirror(t *testing.T) {
+	t.Error("Not implemented")
+}
