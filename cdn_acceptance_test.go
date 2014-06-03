c17815a62142d14df3ed2f0615199890acec47b0
Dan Carley <dan.carley@gmail.com>
2014-06-03 13:07:37 +0100

    Implement some of TestProtocolRedirect
    But not the part that tests the request doesn't hit origin, to confirm that
    the redirect is being served by edge. For this we'll need to start up an
    HTTP server and configure the CDN.

--- cdn_acceptance_test.go
+++ cdn_acceptance_test.go
@@ -2,12 +2,31 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"testing"
+	"net/http"
 )
 
 var edgeHostName = flag.String("edge", "www.gov.uk", "Hostname of edge")
 
 // Should redirect from HTTP to HTTPS without hitting origin.
 func TestProtocolRedirect(t *testing.T) {
-	t.Fatal("Not yet implemented")
+	sourceUrl := fmt.Sprintf("http://%s/", *edgeHostName)
+	destUrl := fmt.Sprintf("https://%s/", *edgeHostName)
+
+	client := &http.Transport{}
+	req, _ := http.NewRequest("GET", sourceUrl, nil)
+	resp, err := client.RoundTrip(req)
+
+	if err != nil {
+		t.Error(err)
+	}
+	if resp.StatusCode != 301 {
+		t.Errorf("Status code expected 301, got %s", resp.StatusCode)
+	}
+	if d := resp.Header.Get("Location"); d != destUrl {
+		t.Errorf("Location header expected %s, got %s", destUrl, d)
+	}
+
+	t.Error("Not implemented test to confirm that it doesn't hit origin")
 }
