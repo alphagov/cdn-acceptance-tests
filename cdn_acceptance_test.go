8c042791ea02888cb4e492bf207c7c5c65706d54
Dan Carley <dan.carley@gmail.com>
2014-06-03 13:27:22 +0100

    Empty tests for headers.

--- cdn_acceptance_test.go
+++ cdn_acceptance_test.go
@@ -30,3 +30,24 @@ func TestProtocolRedirect(t *testing.T) {
 
 	t.Error("Not implemented test to confirm that it doesn't hit origin")
 }
+
+// Should create an X-Forwarded-For header containing the client's IP.
+func TestHeaderCreateXFF(t *testing.T) {
+	t.Error("Not implemented")
+}
+
+// Should append client's IP to existing X-Forwarded-For header.
+func TestHeaderAppendXFF(t *testing.T) {
+	t.Error("Not implemented")
+}
+
+// Should create a True-Client-IP header containing the client's IP
+// address, discarding the value provided in the original request.
+func TestHeaderUnspoofableClientIP(t *testing.T) {
+	t.Error("Not implemented")
+}
+
+// Should not modify Host header from original request.
+func TestHeaderHostUnmodified(t *testing.T) {
+	t.Error("Not implemented")
+}
