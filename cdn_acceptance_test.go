260a862dc4502263649e772c550cff348b52b0d3
Dan Carley <dan.carley@gmail.com>
2014-06-03 11:55:39 +0100

    First test for protocol redirects
    Currently fails. Implementation to follow.

---
+++ cdn_acceptance_test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"testing"
+)
+
+// Should redirect from HTTP to HTTPS without hitting origin.
+func TestProtocolRedirect(t *testing.T) {
+	t.Fatal("Not yet implemented")
+}
