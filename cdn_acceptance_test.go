163917ed7e870d20f584dfcb595119dcc29429dd
Dan Carley <dan.carley@gmail.com>
2014-06-03 12:03:17 +0100

    Add -edge flag
    So that we can configure the target hostname of the edge.

--- cdn_acceptance_test.go
+++ cdn_acceptance_test.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"flag"
 	"testing"
 )
+
+var edgeHostName = flag.String("edge", "www.gov.uk", "Hostname of edge")
 
 // Should redirect from HTTP to HTTPS without hitting origin.
 func TestProtocolRedirect(t *testing.T) {
