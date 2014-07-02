e196910cb44ac58ca0fc584095122658461194a0
Dan Carley <dan.carley@gmail.com>
2014-07-02 12:37:02 +0100

    Use third-party repo for Varnish on Precise
    Precise only has Varnish 3.0.2 which behaves differently from 3.0.5 in
    Trusty. So use a third-party repo to match Trusty.
    
    Normally I'd use puppetlabs-apt to do all of this. But it's too much work to
    pull in bundler and librarian-puppet for this project.

--- mock_cdn_config/modules/varnish/manifests/init.pp
+++ mock_cdn_config/modules/varnish/manifests/init.pp
@@ -1,4 +1,25 @@
 class varnish {
+  # Varnish 3 for Travis CI.
+  if $::lsbdistcodename == "precise" {
+    $apt_key = 'C4DEFFEB'
+
+    exec { 'apt-key varnish':
+      command => "/usr/bin/apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ${apt_key}",
+      unless  => "/usr/bin/apt-key adv --list-keys ${apt_key}",
+    } ->
+    package { 'apt-transport-https':
+      ensure => present,
+    } ->
+    file { '/etc/apt/sources.list.d/varnish.list':
+      content => "deb https://repo.varnish-cache.org/debian/ $::lsbdistcodename varnish-3.0",
+    } ~>
+    exec { 'update apt':
+      command     => '/usr/bin/apt-get update',
+      refreshonly => true,
+    } ->
+    Package['varnish']
+  }
+
   package { 'varnish':
     ensure => present,
   } ->
