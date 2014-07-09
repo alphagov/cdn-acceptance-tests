3ff94342c1d3e73e12456b080e83c6b496cb7e62
Dan Carley <dan.carley@gmail.com>
2014-07-09 14:58:27 +0100

    Implement HealthCheckNotExpiredReplaceStale
    Pair w/Cal.

--- cdn_failover_test.go
+++ cdn_failover_test.go
@@ -107,7 +107,78 @@ func TestFailoverOrigin5xxBackOff(t *testing.T) {
 // FIXME: This is not desired behaviour. We should serve from stale
 //        immediately and not replace the stale object in cache.
 func TestFailoverOriginDownHealthCheckNotExpiredReplaceStale(t *testing.T) {
-	t.Skip("Not implemented")
+	ResetBackends(backendsByPriority)
+
+	const expectedResponseStale = "going off like stilton"
+	const expectedResponseFresh = "as fresh as daisies"
+
+	const respTTL = time.Duration(2 * time.Second)
+	const respTTLWithBuffer = 5 * respTTL
+	headerValue := fmt.Sprintf("max-age=%.0f", respTTL.Seconds())
+
+	backupServer2.SwitchHandler(func(w http.ResponseWriter, r *http.Request) {
+		name := backupServer2.Name
+		t.Errorf("Server %s received request and it shouldn't have", name)
+		w.Write([]byte(name))
+	})
+
+	req := NewUniqueEdgeGET(t)
+
+	var expectedBody string
+	for requestCount := 1; requestCount < 4; requestCount++ {
+		switch requestCount {
+		case 1: // Request 1 populates cache.
+			expectedBody = expectedResponseStale
+
+			originServer.SwitchHandler(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Cache-Control", headerValue)
+				w.Write([]byte(expectedBody))
+			})
+			backupServer1.SwitchHandler(func(w http.ResponseWriter, r *http.Request) {
+				name := backupServer1.Name
+				t.Errorf("Server %s received request and it shouldn't have", name)
+				w.Write([]byte(name))
+			})
+		case 2: // Request 2 comes from mirror and invalidates stale.
+			time.Sleep(respTTLWithBuffer)
+			expectedBody = expectedResponseFresh
+
+			originServer.Stop()
+			backupServer1.SwitchHandler(func(w http.ResponseWriter, r *http.Request) {
+				w.Write([]byte(expectedBody))
+			})
+		case 3: // Request 3 still comes from cache when origin is back.
+			expectedBody = expectedResponseFresh
+
+			ResetBackends(backendsByPriority)
+			originServer.SwitchHandler(func(w http.ResponseWriter, r *http.Request) {
+				name := originServer.Name
+				t.Errorf("Server %s received request and it shouldn't have", name)
+				w.Write([]byte(name))
+			})
+			backupServer1.SwitchHandler(func(w http.ResponseWriter, r *http.Request) {
+				name := backupServer1.Name
+				t.Errorf("Server %s received request and it shouldn't have", name)
+				w.Write([]byte(name))
+			})
+		}
+
+		resp := RoundTripCheckError(t, req)
+
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if bodyStr := string(body); bodyStr != expectedBody {
+			t.Errorf(
+				"Request %d received incorrect response body. Expected %q, got %q",
+				requestCount,
+				expectedBody,
+				bodyStr,
+			)
+		}
+	}
 }
 
 // Should serve stale object and not hit mirror(s) if origin is down, health
