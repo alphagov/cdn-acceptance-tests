df67c7555a51ab37a1b6540021fc2a17736cca71
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2014-08-07 13:32:51 +0100

    Allow jenkins to skip failover tests

--- jenkins.sh
+++ jenkins.sh
@@ -5,5 +5,8 @@ if [ "$TEST_VERIFYTLS" = "yes" ]; then
 else
     export TEST_ARGS="-skipVerifyTLS";
 fi
+if [ "$TEST_FAILOVER" = "no" ]; then
+    export TEST_ARGS="${TEST_ARGS} -skipFailover"
+fi
 echo "Running: go test -edgeHost $TEST_EDGEHOST $TEST_ARGS"
 go test -edgeHost $TEST_EDGEHOST $TEST_ARGS
