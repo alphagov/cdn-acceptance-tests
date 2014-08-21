190cb02eb2835f46cd09db53a7b5d5af5e8e3c3c
Matt Bostock <matt@mattbostock.com>
2014-08-21 10:17:20 +0100

    Remove Makefile
    Remove the Makefile because:
    
      - it removes a dependency on `make(1)`
      - a lot of the tasks, e.g. `vagrant-up`, add little value
      - the `RUN` variable unnecessarily adds the `.+` regex; the default is
        to run all tests anyway
      - as we add more flags (i.e. vendor-specific flags), using a Makefile
        obscures these options

--- travis-ci.sh
+++ travis-ci.sh
@@ -4,7 +4,7 @@ set -eu
 
 sudo FACTER_varnish_backend_address="127.0.0.1" \
   puppet apply --detailed-exitcodes \
-  --modulepath mock_cdn_config/modules mock_cdn_config/manifests/site.pp \
-  || [ $$? -eq 2 ]
+  --modulepath mock_cdn_config/modules \
+  mock_cdn_config/manifests/site.pp || [ $? -eq 2 ]
 
 go test -edgeHost 127.0.0.1 -skipVerifyTLS -v
