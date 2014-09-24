a62a0ef3c5b13a682254473c4666b4314a79433e
Dan Carley <dan.carley@gmail.com>
2014-09-24 10:19:29 +0100

    Merge the two ServeStaleOriginDown tests
    I split these into two tests in 7cce799 because we found that the behaviour
    was different depending on whether you wait for the health check to expire
    or not. The intention was to stop the tests failing and document the nuances
    of the current behaviour.
    
    In hindsight I think this was a mistake. They are confusing to read and make
    it unclear what our *desired* behaviour is. If we're unable to get the right
    implementation but we're confident about the test implementation then we
    should use `t.Skip(notSupportedByVendor)` in the meantime, as we have done
    for some other tests recently.
    
    So merge them back into a single test for serving stale when origin is down.
    We use `respTTLWithBuffer`, like the other stale test, to ensure that we're
    well beyond the object's normal TTL. We do this before stopping the origin
    to ensure that we're not relying on health check expiry.
    
    The one difference now between the OriginDown and Origin5xx tests are that
    the latter also checks that the stale object isn't used when origin comes
    back. We might want to implement this for OriginDown later. But I suspect
    that it may be complicated by removing `checkForSkipFailover()` from these
    tests - which I'll explain in a following commit.
    
    NB: The old test relied on `healthCheckExpire` to also ensure that we were
    beyond the TTL. This was a mistake - it just happened that the expire time
    was longer than the TTL.

--- cdn_serve_stale_test.go
+++ cdn_serve_stale_test.go
@@ -8,98 +8,15 @@ import (
 	"time"
 )
 
-// Should serve reponse from first mirror and replace stale object if origin
-// is down and health check has *not* expired.
-// FIXME: This is not desired behaviour. We should serve from stale
-//        immediately and not replace the stale object in cache.
-func TestServeStaleOriginDownHealthCheckNotExpiredReplace(t *testing.T) {
-	checkForSkipFailover(t)
-	ResetBackends(backendsByPriority)
-
-	const expectedResponseStale = "going off like stilton"
-	const expectedResponseFresh = "as fresh as daisies"
-
-	const respTTL = time.Duration(2 * time.Second)
-	const respTTLWithBuffer = 5 * respTTL
-	headerValue := fmt.Sprintf("max-age=%.0f", respTTL.Seconds())
-
-	backupServer2.SwitchHandler(func(w http.ResponseWriter, r *http.Request) {
-		name := backupServer2.Name
-		t.Errorf("Server %s received request and it shouldn't have", name)
-		w.Write([]byte(name))
-	})
-
-	req := NewUniqueEdgeGET(t)
-
-	var expectedBody string
-	for requestCount := 1; requestCount < 4; requestCount++ {
-		switch requestCount {
-		case 1: // Request 1 populates cache.
-			expectedBody = expectedResponseStale
-
-			originServer.SwitchHandler(func(w http.ResponseWriter, r *http.Request) {
-				w.Header().Set("Cache-Control", headerValue)
-				w.Write([]byte(expectedBody))
-			})
-			backupServer1.SwitchHandler(func(w http.ResponseWriter, r *http.Request) {
-				name := backupServer1.Name
-				t.Errorf("Server %s received request and it shouldn't have", name)
-				w.Write([]byte(name))
-			})
-		case 2: // Request 2 comes from mirror and invalidates stale.
-			time.Sleep(respTTLWithBuffer)
-			expectedBody = expectedResponseFresh
-
-			originServer.Stop()
-			backupServer1.SwitchHandler(func(w http.ResponseWriter, r *http.Request) {
-				w.Write([]byte(expectedBody))
-			})
-		case 3: // Request 3 still comes from cache when origin is back.
-			expectedBody = expectedResponseFresh
-
-			ResetBackends(backendsByPriority)
-			originServer.SwitchHandler(func(w http.ResponseWriter, r *http.Request) {
-				name := originServer.Name
-				t.Errorf("Server %s received request and it shouldn't have", name)
-				w.Write([]byte(name))
-			})
-			backupServer1.SwitchHandler(func(w http.ResponseWriter, r *http.Request) {
-				name := backupServer1.Name
-				t.Errorf("Server %s received request and it shouldn't have", name)
-				w.Write([]byte(name))
-			})
-		}
-
-		resp := RoundTripCheckError(t, req)
-		defer resp.Body.Close()
-
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if bodyStr := string(body); bodyStr != expectedBody {
-			t.Errorf(
-				"Request %d received incorrect response body. Expected %q, got %q",
-				requestCount,
-				expectedBody,
-				bodyStr,
-			)
-		}
-	}
-}
-
-// Should serve stale object and not hit mirror(s) if origin is down, health
-// check has expired, and object is beyond TTL but still in cache.
-// FIXME: This is not quite desired behaviour. We should not have to wait
-//				for the stale object to become available.
-func TestServeStaleOriginDownHealthCheckHasExpired(t *testing.T) {
+// Should serve stale object and not hit mirror(s) if origin is down and
+// object is beyond TTL but still in cache.
+func TestServeStaleOriginDown(t *testing.T) {
 	checkForSkipFailover(t)
 	ResetBackends(backendsByPriority)
 
 	const expectedBody = "going off like stilton"
-	// Allow health check to expire. Depends on window/threshold/interval.
-	const healthCheckExpire = time.Duration(20 * time.Second)
 	const respTTL = time.Duration(2 * time.Second)
+	const respTTLWithBuffer = 5 * respTTL
 	headerValue := fmt.Sprintf("max-age=%.0f", respTTL.Seconds())
 
 	backupServer1.SwitchHandler(func(w http.ResponseWriter, r *http.Request) {
@@ -122,9 +39,9 @@ func TestServeStaleOriginDownHealthCheckHasExpired(t *testing.T) {
 				w.Header().Set("Cache-Control", headerValue)
 				w.Write([]byte(expectedBody))
 			})
-		case 2: // Request 2 come from stale.
+		case 2: // Request 2+ from stale.
+			time.Sleep(respTTLWithBuffer)
 			originServer.Stop()
-			time.Sleep(healthCheckExpire)
 		}
 
 		resp := RoundTripCheckError(t, req)
